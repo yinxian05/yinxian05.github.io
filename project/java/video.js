73064aa4bffb09b0852ce57ff9266e996a5d1f85
Saw Yin Xian <yinxian241010@dasein.edu.my>
2025-05-18 21:27:38 +0800

    updated

--- project/java/video.js
+++ project/java/video.js
@@ -1,4 +1,4 @@
-const toggleButton = document.getElementById("menu-toggle");
+toggleButton = document.getElementById("menu-toggle");
 const menu = document.querySelector(".section"); // Target the element with the class 'section'
 
 toggleButton.addEventListener("click", () => {
@@ -8,6 +8,7 @@ toggleButton.addEventListener("click", () => {
     if (menu.classList.contains("active")) {
         toggleButton.innerHTML = "&times;"; // × (cross)
     } else {
+        const
         toggleButton.innerHTML = "&#9776;"; // ☰ (hamburger)
     }
 });
